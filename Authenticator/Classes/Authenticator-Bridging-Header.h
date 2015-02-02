233f82826dd3eb3ae52459225545b383b1c14d9f
Matt Rubin <opensource@mattrubin.me>
2015-02-02 05:21:26 -0500

    Compile SVProgressHUD directly in the app

--- Authenticator/Classes/Authenticator-Bridging-Header.h
+++ Authenticator/Classes/Authenticator-Bridging-Header.h
@@ -2,7 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <SVProgressHUD/SVProgressHUD.h>
+#import "SVProgressHUD.h"
+
 #import "OTPTokenCell.h"
 #import "OTPTokenEditViewController.h"
 #import "OTPScannerViewController.h"
